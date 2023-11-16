bb8dbde570b8b884d91579daf0b4f18da972af3a
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-11-16 13:11:50 -0500

    Adds new db schema png to server directory. Updates models.py to reflect the new schema. Missing validations from all classes except users.

--- server/models.py
+++ server/models.py
@@ -12,14 +12,12 @@ db = SQLAlchemy(metadata=metadata)
 
 class User(db.Model, SerializerMixin):
   __tablename__ = 'users'
-  serialize_rules = ('-save_files.user', '-save_files.user_fk')
+  # serialize_rules = (,)
 
   id = db.Column(db.Integer, primary_key=True)
+
   username = db.Column(db.String, unique=True)
   password = db.Column(db.String)
-
-  save_files = db.relationship('SaveFile', back_populates='user')
-
 
   @validates('username')
   def validates_username(self, key, username):
@@ -37,85 +35,111 @@ class User(db.Model, SerializerMixin):
     
   def __repr__(self):
     return f'<User {self.id}>'
-
-
-
-class UserAchievement(db.Model, SerializerMixin):
-  __tablename__ = 'users_achievements'
-  # serialize_rules = 
+  
+class Character(db.Model, SerializerMixin):
+  __tablename__ = 'characters'
+  # serialize_rules = (,)
 
   id = db.Column(db.Integer, primary_key=True)
   user_fk = db.Column(db.Integer, db.ForeignKey('users.id'))
-  achievement_fk = db.Column(db.Integer, db.ForeignKey('achievements.id'))
 
-  @validates('user_fk')
-  def validates_user_fk(self, key, user_fk):
-    if user_fk:
-      return user_fk
-    else:
-      raise ValueError('UserAchievement must be given a user foreign key.')
-    
-  @validates('achievement_fk')
-  def validates_achievement_fk(self, key, achievement_fk):
-    if achievement_fk:
-      return achievement_fk
-    else:
-      raise ValueError('UserAchievement must be given an achievement foreign key.')
-
-  def __repr__(self):
-    return f'<UserAchievement {self.id}>'
-
-
-
-class Achievement(db.Model, SerializerMixin):
-  __tablename__ = 'achievements'
-  # serialize_rules = 
-
-  id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String)
-
-  @validates('name')
-  def validates_name(self, key, name):
-    if name:
-      return name
-    else:
-      raise ValueError('Achievement must be given a name.')
+  datetime_created = db.Column(db.String)
+  datetime_last_played = db.Column(db.String)
 
   def __repr__(self):
-    return f'<Achievement {self.id}>'
-
+      return f'<Character {self.id}>'
 
 class SaveFile(db.Model, SerializerMixin):
   __tablename__ = 'save_files'
-  serialize_rules = ('-user',)
+  # serialize_rules = (,)
 
   id = db.Column(db.Integer, primary_key=True)
-  src = db.Column(db.String)
-  timestamp = db.Column(db.String)
-  user_fk = db.Column(db.Integer, db.ForeignKey('users.id'))
+  character_fk = db.Column(db.Integer, db.ForeignKey('characters.id'))
 
-  user = db.relationship('User', back_populates='save_files')
-
-  @validates('src')
-  def validates_src(self, key, src):
-    if src:
-      return src
-    else:
-      raise ValueError('SaveFile must be given a src.')
-    
-  @validates('timestamp')
-  def validates_timestamp(self, key, timestamp):
-    if timestamp:
-      return timestamp
-    else:
-      raise ValueError('SaveFile must be given a timestamp.')
-
-  @validates('user_fk')
-  def validates_user_fk(self, key, user_fk):
-    if user_fk:
-      return user_fk
-    else:
-      raise ValueError('SaveFile must be given a user foreign key.')
+  datetime_created = db.Column(db.String)
+  location_on_save = db.Column(db.String)
+  has_entered_portal = db.Column(db.String)
+  met_old_man = db.Column(db.String)
+  met_girl = db.Column(db.String)
+  found_girls_item = db.Column(db.String)
+  girls_item_location = db.Column(db.String)
+  mini_game_high_score = db.Column(db.Integer)
+  has_visited_store = db.Column(db.String)
+  gold_pieces = db.Column(db.Integer)
+  met_village2_trader = db.Column(db.String)
+  accepted_quest_village2_trader = db.Column(db.String)
+  met_village1_trade_target = db.Column(db.String)
+  negotiated_deal = db.Column(db.String)
+  wizard_is_home = db.Column(db.String)
   
   def __repr__(self):
     return f'<SaveFile {self.id}>'
+  
+class SaveItemJoin(db.Model, SerializerMixin):
+  __tablename__ = 'saves_items_joins'
+  # serialize_rules = (,)
+
+  id = db.Column(db.Integer, primary_key=True)
+
+  item_name = db.Column(db.String)
+
+  def __repr__(self):
+      return f'<Character {self.id}>'
+  
+class InventoryItem(db.Model, SerializerMixin):
+  __tablename__ = 'inventory_items'
+  # serialize_rules = (,)
+
+  id = db.Column(db.Integer, primary_key=True)
+  save_file_fk = db.Column(db.Integer, db.ForeignKey('save_files.id'))
+  inventory_item_fk = db.Column(db.Integer, db.ForeignKey('inventory_items.id'))
+
+  def __repr__(self):
+      return f'<Character {self.id}>'
+
+
+
+  # class UserAchievement(db.Model, SerializerMixin):
+#   __tablename__ = 'users_achievements'
+#   # serialize_rules = 
+
+#   id = db.Column(db.Integer, primary_key=True)
+#   user_fk = db.Column(db.Integer, db.ForeignKey('users.id'))
+#   achievement_fk = db.Column(db.Integer, db.ForeignKey('achievements.id'))
+
+#   @validates('user_fk')
+#   def validates_user_fk(self, key, user_fk):
+#     if user_fk:
+#       return user_fk
+#     else:
+#       raise ValueError('UserAchievement must be given a user foreign key.')
+    
+#   @validates('achievement_fk')
+#   def validates_achievement_fk(self, key, achievement_fk):
+#     if achievement_fk:
+#       return achievement_fk
+#     else:
+#       raise ValueError('UserAchievement must be given an achievement foreign key.')
+
+#   def __repr__(self):
+#     return f'<UserAchievement {self.id}>'
+
+
+
+# class Achievement(db.Model, SerializerMixin):
+#   __tablename__ = 'achievements'
+#   # serialize_rules = 
+
+#   id = db.Column(db.Integer, primary_key=True)
+#   name = db.Column(db.String)
+
+#   @validates('name')
+#   def validates_name(self, key, name):
+#     if name:
+#       return name
+#     else:
+#       raise ValueError('Achievement must be given a name.')
+
+#   def __repr__(self):
+#     return f'<Achievement {self.id}>'
