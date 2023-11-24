4b6c7cc48a79fac6778b78d5243fc45e00c4ab22
jessicatwitmyer <jatwitmyer@gmail.com>
2023-11-23 23:08:42 -0500

    notes for clarity:

--- client/src/components/StartGame.js
+++ client/src/components/StartGame.js
@@ -65,30 +65,33 @@ function StartGame( { selectedCharacter, selectedSaveFile } ) {
   }
   // console.log(event)
 
+  //  !!!!  each event must have storage and id properties to function !!!!
   const landslide = {
+    storage: "incomplete_events",
     id: 1,
     setup: "You've walked on this trail many times before. It's your favorite for clearing your thoughts. Today, you can't get the feeling that your sister's making a mistake out of your head. What will you say to her? How do you know it will reach her?\n\nYou're pulled to the present by the sound of the birds going silent. A chill goes up your spine. From the trail behind you, you hear a low rumble growing, but from what? Is it a rockslide or a moose?",
     choices: [
       {id: "A", selection: "Leave the path to hide in the forest", setup: "You rush off the trail. Once you reach a hiding spot, you hear tree branches snapping in the distance. A rock slams into the road 50 feet away from you and breaks apart.", choices: [
-        {id: "A", selection: "Run into the forest away from the rock", closer:"You race into the trees. Behind you, rocks and broken trees begin to fill the path. You're blocked from the exit of the park. It will take hours for the way to be cleared. You sigh and continue forward into the forest. If you go straight, you know you'll reach the edge before sunset.", eventId: 1},
-        {id: "B", selection:"Go back to the road to run away faster", closer:"You race down the trail. Behind you, rocks and broken trees begin to fill the path. You're blocked from the exit of the park. It will take hours for the way to be cleared. You sigh and turn to enter the forest. If you go straight, you know you'll reach the edge before sunset.", eventId: 1}
+        {id: "A", selection: "Run into the forest away from the rock", closer:"You race into the trees. Behind you, rocks and broken trees begin to fill the path. You're blocked from the exit of the park. It will take hours for the way to be cleared. You sigh and continue forward into the forest. If you go straight, you know you'll reach the edge before sunset."},
+        {id: "B", selection:"Go back to the road to run away faster", closer:"You race down the trail. Behind you, rocks and broken trees begin to fill the path. You're blocked from the exit of the park. It will take hours for the way to be cleared. You sigh and turn to enter the forest. If you go straight, you know you'll reach the edge before sunset."}
       ]},
-      {id: "B", selection: "Bolt down the trail to put distance between you and the sound", closer: "You race down the trail. You lose your breath and turn back to check on the noise. You hear tree branches snapping in the distance. Behind you, rocks and broken trees begin to fill the path. You're blocked from the exit of the park. It will take hours for the way to be cleared. You sigh and turn to enter the forest. If you go straight, you know you'll reach the edge before sunset.", eventId: 1},
+      {id: "B", selection: "Bolt down the trail to put distance between you and the sound", closer: "You race down the trail. You lose your breath and turn back to check on the noise. You hear tree branches snapping in the distance. Behind you, rocks and broken trees begin to fill the path. You're blocked from the exit of the park. It will take hours for the way to be cleared. You sigh and turn to enter the forest. If you go straight, you know you'll reach the edge before sunset."},
     ],
-    completed: false
+    achievementId: 1
   }
   // console.log(landslide) //achievement: lived to tell the tale
   
   const findPortal = {
+    storage: "incomplete_events",
     id: 2,
     setup: "You're slowly making your way through the forest when you hear something ahead of you. It sounds like a windchime. ",
     choices: [
-      {choice: "", closer: ""},
-      {choice: "", setup: "", choices: [
-        {choice: "", closer: ""},
-        {choice: "", closer: ""}
+      {choice: "text", closer: "text"},
+      {choice: "text", setup: "text", choices: [
+        {choice: "text", closer: "text"},
+        {choice: "text", closer: "text"}
       ]}],
-    completed: false
+    achievementId: 2
   }
   // console.log(findPortal)
   
